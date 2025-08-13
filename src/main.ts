56f7cf45ace8a7ebc1cbd036b41b30588d3bee5b
mharoondev <m.haroon@paz.agency>
2025-08-13 17:06:42 +0500

    refactor: update package.json scripts for production and Vercel deployment
    - Changed the "start" script to run the compiled application directly.
    - Added a "vercel-build" script to build the application before deployment.
    - Modified main.ts to cache the server instance for improved performance.
    - Updated Vercel configuration to use the compiled main.js file for builds and routing.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    Origin: "*",
-  });
-  await app.listen(process.env.PORT ?? 3000);
+let server: any; // cache server instance to avoid re-bootstrapping every request
+
+export default async function handler(req: any, res: any) {
+  if (!server) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors(); // Allow cross-origin requests if needed
+    await app.init();
+    server = app.getHttpAdapter().getInstance();
+  }
+  return server(req, res);
 }
-bootstrap();
